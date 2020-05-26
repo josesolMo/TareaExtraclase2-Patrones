872003541d402c8220564b4781ae4a2bf4d11ac6
Jose Solano <joseandressolanomora@gmail.com>
2020-05-26 11:43:04 -0600

    Se integra el patrón Abstract Factory

---
+++ abstract_factory_ej/cmake-build-debug/CMakeFiles/abstract_factory_ej.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/abstract_factory_ej.dir/main.cpp.o"
+  "CMakeFiles/abstract_factory_ej.dir/Car.cpp.o"
+  "CMakeFiles/abstract_factory_ej.dir/CarFactory.cpp.o"
+  "abstract_factory_ej.pdb"
+  "abstract_factory_ej"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/abstract_factory_ej.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
